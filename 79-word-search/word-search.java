5a08da6ab8e5346685a902ba40579b63f7ce3fac
Harshil Sharma <harshils3107@gmail.com>
2026-05-16 14:56:39 +0530

    Time: 194 ms (20.54%) | Memory: 43.2 MB (30.45%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,31 +1,30 @@
 class Solution {
-    int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    int[][] directions ={{1,0},{-1,0},{0,1},{0,-1}};
     public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0) && find(board, word, i, j, 0)) {
+        for(int i=0;i<board.length;i++) {
+            for(int j=0;j<board[0].length;j++) {
+                if( word.charAt(0)==board[i][j] && findWord(board,i,j,word,0)) {
                     return true;
                 }
             }
         }
         return false;
+
+        
     }
-    boolean find(char[][] board, String word, int i, int j, int idx) {
-        if (idx == word.length()) return true;
-        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(idx)) {
-            return false;
+    boolean findWord(char[][] board,int i,int j,String word,int idx) {
+        if(idx==word.length()) return true;
+        if(i<0 || j<0 || i>=board.length || j>=board[0].length || board[i][j]=='$' || word.charAt(idx)!=board[i][j]) return false;
+        char temp=board[i][j];
+        board[i][j]='$';
+        for(int[] d:directions) {
+            int i1=i+d[0];
+            int j1=j+d[1];
+            if(findWord(board,i1,j1,word,idx+1)) return true;    
         }
-        char temp = board[i][j];
-        board[i][j] = '$'; 
-        for (int[] dir : directions) {
-            int i1 = i + dir[0];
-            int j1 = j + dir[1];
-            if (find(board, word, i1, j1, idx + 1)) return true;
-        }
-        board[i][j] = temp;
+        board[i][j]=temp;
         return false;
+
+
     }
 }
