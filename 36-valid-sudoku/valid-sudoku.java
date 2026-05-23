9eeebedf1d7dfa375bcd82e8cc45e6f097ca9206
Harshil Sharma <harshils3107@gmail.com>
2026-05-23 21:42:04 +0530

    Time: 1 ms (99.93%) | Memory: 46.5 MB (57.22%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,34 +1,33 @@
 class Solution {
-    public boolean isValidSudoku(char[][] arr) {
-        for (int i = 0; i < 9; i++) {
-            HashMap<Character, Integer> map1 = new HashMap<>();
-            HashMap<Character, Integer> map2 = new HashMap<>();
-            for (int j = 0; j < 9; j++) {
-                if (arr[i][j] != '.') {
-                    map1.put(arr[i][j], map1.getOrDefault(arr[i][j], 0) + 1);
-                    if (map1.get(arr[i][j]) > 1) return false;
-                }
-                if (arr[j][i] != '.') {
-                    map2.put(arr[j][i], map2.getOrDefault(arr[j][i], 0) + 1);
-                    if (map2.get(arr[j][i]) > 1) return false;
+    public boolean isValidSudoku(char[][] board) {
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++) {
+                if(board[i][j]!='.') {
+                    if((isValid(board,board[i][j],i,j))==true) continue;
+                    else return false;
                 }
             }
         }
-        for (int b = 0; b < 9; b++) {
-            HashMap<Character, Integer> map3 = new HashMap<>();
-            int startRow = (b / 3) * 3;
-            int startCol = (b % 3) * 3;
-            
-            for (int r = startRow; r < startRow + 3; r++) {
-                for (int c = startCol; c < startCol + 3; c++) {
-                    if (arr[r][c] != '.') {
-                        map3.put(arr[r][c], map3.getOrDefault(arr[r][c], 0) + 1);
-                        if (map3.get(arr[r][c]) > 1) return false;
-                    }
-                }
-            }
-        }
+        return true;    
+    }
 
+    boolean isValid(char[][] board,char ch,int i,int j) {
+        int rowCount=0;
+        int colCount=0;
+        int boxCount=0;
+        for(int k=0;k<9;k++) {
+            if(board[i][k]==ch) rowCount++;
+            if(board[k][j]==ch) colCount++;
+            if(rowCount>1 || colCount>1) return false;
+        }
+        int box_i=(i/3)*3;
+        int box_j=(j/3)*3;
+        for(int k=box_i;k<box_i+3;k++) {
+            for(int l=box_j;l<box_j+3;l++){
+                if(board[k][l]==ch) boxCount++;
+                if(boxCount>1) return false;
+            }
+        }
         return true;
     }
 }
