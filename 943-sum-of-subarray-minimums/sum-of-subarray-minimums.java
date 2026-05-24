1562a03f1cedc55478cc8713d3d384b28dd26869
Harshil Sharma <harshils3107@gmail.com>
2026-05-24 17:57:34 +0530

    Time: 6 ms (99.59%) | Memory: 53.9 MB (77.25%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -2,38 +2,17 @@ class Solution {
     public int sumSubarrayMins(int[] arr) {
         int n = arr.length;
         int MOD = 1000000007;
-        int[] prev = new int[n];
-        int[] next = new int[n];
-        pse(arr, prev);
-        nsee(arr, next);
         long sum = 0;
-        for (int i = 0; i < n; i++) {
-            long left = i - prev[i];
-            long right = next[i] - i;
-            sum = (sum + (long) arr[i] * left % MOD * right) % MOD;
+        int[] stack = new int[n + 1];
+        int top = -1;
+        for (int i = 0; i <= n; i++) {
+            while (top >= 0 && (i == n || arr[stack[top]] >= arr[i])) {
+                int mid = stack[top--];
+                int left = top < 0 ? -1 : stack[top];
+                sum = (sum + (long) arr[mid] * (mid - left) * (i - mid)) % MOD;
+            }
+            stack[++top] = i;
         }
         return (int) sum;
-    }
-
-    void pse(int[] arr, int[] prev) {
-        int n=arr.length;
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i])
-                stack.pop();
-            prev[i] = stack.isEmpty() ? -1 : stack.peek();
-            stack.push(i);
-        }
-    }
-
-    void nsee(int[] arr, int[] next) {
-        int n =arr.length;
-        Stack<Integer> stack = new Stack<>();
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && arr[stack.peek()] > arr[i])
-                stack.pop();
-            next[i] = stack.isEmpty() ? n : stack.peek();
-            stack.push(i);
-        }
     }
 }
