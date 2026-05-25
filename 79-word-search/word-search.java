5ad86331a05033e9dd2d71e249a5f8f984a1616f
Harshil Sharma <harshils3107@gmail.com>
2026-05-25 15:25:50 +0530

    Time: 191 ms (21.55%) | Memory: 43 MB (59.51%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,30 +1,28 @@
 class Solution {
-    int[][] directions ={{1,0},{-1,0},{0,1},{0,-1}};
+    int[][] directions={{0,1},{0,-1},{1,0},{-1,0}};
     public boolean exist(char[][] board, String word) {
         for(int i=0;i<board.length;i++) {
             for(int j=0;j<board[0].length;j++) {
-                if( word.charAt(0)==board[i][j] && findWord(board,i,j,word,0)) {
-                    return true;
-                }
+                if (word.charAt(0)==board[i][j] && search(board,word,0,i,j)) return true;
             }
         }
         return false;
-
         
     }
-    boolean findWord(char[][] board,int i,int j,String word,int idx) {
+    boolean search(char[][] board,String word,int idx,int i,int j) {
         if(idx==word.length()) return true;
-        if(i<0 || j<0 || i>=board.length || j>=board[0].length || board[i][j]=='$' || word.charAt(idx)!=board[i][j]) return false;
+        if(i<0 || j<0 || i>=board.length || j>=board[0].length || idx>=word.length() || board[i][j]=='$' || board[i][j]!=word.charAt(idx)) return false;
         char temp=board[i][j];
         board[i][j]='$';
-        for(int[] d:directions) {
-            int i1=i+d[0];
-            int j1=j+d[1];
-            if(findWord(board,i1,j1,word,idx+1)) return true;    
+        for(int[] dir:directions) {
+            int i1=i+dir[0];
+            int j1=j+dir[1];
+            if (search(board,word,idx+1,i1,j1)) return true;  
+             
         }
         board[i][j]=temp;
         return false;
-
-
+    
     }
+    
 }
