9c30daf2181bf3a3670c1ec52a798215fbdf491f
Harshil Sharma <harshils3107@gmail.com>
2026-05-27 12:48:51 +0530

    Time: 3 ms (86.81%) | Memory: 52.2 MB (59.47%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,5 +1,4 @@
 class Solution {
-    
     public int numIslands(char[][] grid) {
         int islands=0;
         int r=grid.length;
@@ -8,7 +7,7 @@ class Solution {
         for(int i=0;i<r;i++) {
             for(int j=0;j<c;j++) {
                 if(!visited[i][j] && grid[i][j]=='1') {
-                    bfs(grid,visited,i,j);
+                    dfs(grid,visited,i,j);
                     islands++;
                     
                 }
@@ -16,23 +15,18 @@ class Solution {
         } 
         return islands;  
     }
-    void bfs(char[][] grid, boolean[][] visited, int i, int j) {
-        int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        Queue<int[]> q = new LinkedList<>();
-        visited[i][j] = true; 
-        q.add(new int[]{i, j});
-        while (q.size() > 0) {
-            int[] front = q.remove(); 
-            for (int[] d : dir) {
-                int i1 = front[0] + d[0];
-                int j1 = front[1] + d[1];
-                if (i1 >= 0 && i1 < grid.length && j1 >= 0 && j1 < grid[0].length 
-                    && grid[i1][j1] == '1' && !visited[i1][j1]) {
-                    visited[i1][j1] = true; 
-                    q.add(new int[]{i1, j1});
-                }
-            }
-        }
+    void dfs(char[][] grid, boolean[][] visited, int i, int j)  {
+        visited[i][j]=true;
+        if(i-1>=0 && !visited[i-1][j]  && grid[i-1][j]=='1' ) dfs(grid,visited,i-1,j);
+        if(j-1>=0 && !visited[i][j-1]  && grid[i][j-1]=='1' ) dfs(grid,visited,i,j-1);
+        if(i+1<grid.length && !visited[i+1][j] &&  grid[i+1][j]=='1' ) dfs(grid,visited,i+1,j);
+        if(j+1<grid[0].length && !visited[i][j+1]  && grid[i][j+1]=='1' ) dfs(grid,visited,i,j+1);
+
+
+
+
+
     }
+     
 
 }
