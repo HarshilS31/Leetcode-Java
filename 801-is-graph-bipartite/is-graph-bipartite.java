0fe04d543040419f1273cc8b68abb8cace817db3
Harshil Sharma <harshils3107@gmail.com>
2026-05-28 17:46:58 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (56.99%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -6,28 +6,40 @@ class Solution {
         //0->Red,1->Blue
         for(int i=0;i<n;i++) {
             if(colored[i]==-1) {
-                if(!bfs(graph,colored,i)) return false;
+                if(!dfs(graph,colored,i,0)) return false;
             }
         }
         return true;
 
     }
-    boolean bfs(int[][] graph,int[] colored,int idx) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(idx);
-        colored[idx]=0;
-
-        while(q.size()>0) {
-            int front=q.remove();
-            int color=colored[front];
-            for(int node:graph[front]) {
-                if(colored[front]==colored[node]) return false;
-                if(colored[node]==-1) q.add(node);
-                colored[node]=1-colored[front];
-                
+    boolean dfs(int[][] graph,int[] colored,int idx,int color) {
+        colored[idx]=color;
+        for(int node:graph[idx]){
+            if(colored[node]==colored[idx]) return false;
+            if(colored[node]==-1) {
+                if(!dfs(graph,colored,node,1-color)) return false;
             }
         }
-        return true;    
+        return true;
+
+
     }
+    // boolean bfs(int[][] graph,int[] colored,int idx) {
+    //     Queue<Integer> q = new LinkedList<>();
+    //     q.add(idx);
+    //     colored[idx]=0;
+
+    //     while(q.size()>0) {
+    //         int front=q.remove();
+    //         int color=colored[front];
+    //         for(int node:graph[front]) {
+    //             if(colored[front]==colored[node]) return false;
+    //             if(colored[node]==-1) q.add(node);
+    //             colored[node]=1-colored[front];
+                
+    //         }
+    //     }
+    //     return true;    
+    // }
 
 }
