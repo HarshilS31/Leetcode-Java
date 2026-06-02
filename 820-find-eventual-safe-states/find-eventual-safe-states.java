b636d718807fd8834627a2d07a11534b1cc1e60b
Harshil Sharma <harshils3107@gmail.com>
2026-06-02 16:09:45 +0530

    Time: 4 ms (99.73%) | Memory: 63.4 MB (35.04%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        List<Integer> ans = new ArrayList<>();
+        int[] status=new int[graph.length];
+        // 0->unvisited, 1->visiting, 2->visited
+        for(int i=0;i<graph.length;i++) {
+            if(isSafe(i,graph,status)) ans.add(i);
+        }
+        return ans;
+    }
+    public boolean isSafe(int currNode,int[][] graph,int[] status) {
+        if(status[currNode]>0) return status[currNode]==2;
+        status[currNode]=1;
+        for(int node:graph[currNode]) {
+            if(!isSafe(node,graph,status)) return false;
+           
+        }
+        status[currNode]=2;
+        return true;     
+    }
+}
