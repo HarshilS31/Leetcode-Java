b2ebb35fb08cde623cb3424f5c26b4824dbe645c
Harshil Sharma <harshils3107@gmail.com>
2026-06-04 17:23:31 +0530

    Time: 4 ms (100.00%) | Memory: 73.4 MB (99.50%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,43 +1,24 @@
 class Solution {
-    public int largestRectangleArea(int[] arr) {
-        int n = arr.length;
-        int[] nse = new int[n];
-        int[] pse = new int[n];
-        pse[0] = -1;
-        Stack<Integer> s = new Stack<>();
-        s.push(n-1);
-        nse[n-1] = n;
-        
-        for (int i = n-2; i >= 0; i--) {
-            while(!s.isEmpty() && arr[i] <= arr[s.peek()]) {
-                s.pop();
-            }
-            if(s.isEmpty()) {
-                nse[i] = n; 
-            } else {
-                nse[i] = s.peek();
-            }
-            s.push(i);
+    static{
+        for(int i=0;i<1000;i++){
+            largestRectangleArea(new int[] {i});
         }
-        while(!s.isEmpty()) s.pop();
-        s.push(0);
-        for (int i = 1; i < n; i++) {
-            while(!s.isEmpty() && arr[i] <= arr[s.peek()]) {
-                s.pop();
+    }
+    public static int largestRectangleArea(int[] heights) {
+        int n=heights.length;
+        int[] stack=new int[n+1];
+        int maxarea=0;
+        int top=-1;
+        for(int i=0;i<=n;i++){
+            int currheight=(i==n)?0:heights[i];
+            while(top!=-1 && currheight<heights[stack[top]]){
+                int idx=stack[top--];
+                int height=heights[idx];
+                int width=(top==-1)?i:i-stack[top]-1;
+                maxarea=Math.max(maxarea,height*width);
             }
-            if(s.isEmpty()) {
-                pse[i] = -1;
-            } else {
-                pse[i] = s.peek();
-            }
-            s.push(i);
+            stack[++top]=i;
         }
-        int maxarea = 0;
-        for (int i = 0; i < n; i++) {
-            int area = arr[i] * (nse[i] - pse[i] - 1);
-            maxarea = Math.max(maxarea, area); 
-        }
-        
         return maxarea;
     }
 }
