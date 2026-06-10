34624d8ae5d1ea2e848292de49444bf752a7bbe9
Harshil Sharma <228211535+HarshilS31@users.noreply.github.com>
2026-06-10 20:05:17 +0530

    Time: 4 ms (75.41%) | Memory: 45.5 MB (22.10%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,25 +1,24 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] nge=new int[nums1.length];
-        HashMap<Integer,Integer> n1= new HashMap<>();
-        for(int i=0;i<nums1.length;i++) {
-            n1.put(nums1[i],i);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        Stack<Integer> s = new Stack<>();
+        map.put(nums2[nums2.length-1],-1);
+        for(int i=nums2.length-1;i>=0;i--) {
+            while(!s.isEmpty() && nums2[i]>s.peek()) {
+                s.pop();
+            }
+            if(s.isEmpty()) map.put(nums2[i],-1);
+            else map.put(nums2[i],s.peek());
+            s.push(nums2[i]);
+
         }
-        Stack<Integer> temp= new Stack<>();
+        for(int i=0;i<nums1.length;i++) {
+            int nge=map.get(nums1[i]);
+            nums1[i]=nge;
+        }
+        return nums1;
 
        
-        for(int i=nums2.length-1;i>=0;i--) {
-            while(temp.size()>0 && nums2[i]>temp.peek()) {
-                temp.pop();
-            }
-            if(n1.containsKey(nums2[i])) {
-                nge[n1.get(nums2[i])]= temp.size()>0? temp.peek():-1;
-            }
-            temp.push(nums2[i]);
-        }
-
         
-        return nge;
     }
-
 }
