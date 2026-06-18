919304af3a2125b43be1e7f98733d471e1d64818
Harshil Sharma <228211535+HarshilS31@users.noreply.github.com>
2026-06-18 12:28:17 +0530

    Time: 2 ms (43.71%) | Memory: 47.3 MB (42.31%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -2,24 +2,35 @@ class Solution {
     public int findCircleNum(int[][] adj) {
         int count=0;
         int n=adj.length;
-        boolean[] visited=new boolean[n];
+        int[] parent = new int[n+1]; 
+        for(int i=1;i<=n;i++) parent[i]=i;
+        parent[0]=Integer.MIN_VALUE;
+        List<List<Integer>> edges = new ArrayList<>();
         for(int i=0;i<n;i++) {
-            if(!visited[i]) {
-                dfs(adj,visited,i);
-                count++;
+            for(int j=0;j<n;j++) { 
+                if(adj[i][j]==1 && i!=j) {
+                    union(i+1,j+1,parent);
+                } 
             }
+        }
+        for(int i=1;i<=n;i++) {
+            if(parent[i]==i) count++;
         }
         return count;
+        
     }
-    void dfs(int[][] adj,boolean[] visited,int idx) {
-        int n=adj.length;
-        visited[idx]=true;
-        for(int j=0;j<n;j++) {
-            if(adj[idx][j]==1 && !visited[j]) {
-                dfs(adj,visited,j);
-            }
+    public void union(int x,int y,int[] parent) {
+        int ultimateP_x=find(x,parent);
+        int ultimateP_y=find(y,parent);
+        if(ultimateP_x!=ultimateP_y) {
+            parent[ultimateP_y]=ultimateP_x;
+
         }
 
+    }
+    public static int find(int node,int[] parent) {
+        if(node==parent[node]) return node;
+        return find(parent[node],parent);
 
     }
 }
