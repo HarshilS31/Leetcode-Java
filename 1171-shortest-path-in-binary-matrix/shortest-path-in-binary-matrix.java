a452a7edfc58674a8d98574c2c51e4eb5eb96467
Harshil Sharma <228211535+HarshilS31@users.noreply.github.com>
2026-06-19 16:37:22 +0530

    Time: 18 ms (31.57%) | Memory: 48.1 MB (13.11%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -5,13 +5,13 @@ class Solution {
         if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) return -1;
         int[][] distance = new int[n][n];
         for(int i=0;i<n;i++) Arrays.fill(distance[i],Integer.MAX_VALUE);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->Integer.compare(a[2],b[2]));
+        Queue<int[]> q = new LinkedList<>();
         int[][] directions ={{0,1},{0,-1},{1,0},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
-        boolean[][] visited = new boolean[n][n];
-        pq.add(new int[]{0,0,1});
-        visited[0][0]=true;
-        while(pq.size()>0) {
-            int[] top = pq.poll();
+  
+        q.add(new int[]{0,0,1});
+
+        while(q.size()>0) {
+            int[] top = q.poll();
             int x=top[0];
             int y=top[1];
             int d=top[2];
@@ -20,13 +20,13 @@ class Solution {
                 int x1=x+dir[0];
                 int y1=y+dir[1];
                 
-                if(x1>=0 && y1>=0 && x1<n && y1<n && grid[x1][y1]==0 && !visited[x1][y1]) {
+                if(x1>=0 && y1>=0 && x1<n && y1<n && grid[x1][y1]==0 ) {
                     int dist=d+1;
                     if(distance[x1][y1]>dist) {
                         distance[x1][y1]=dist;
-                        pq.add(new int[]{x1,y1,dist});
+                        q.add(new int[]{x1,y1,dist});
                     }
-                    visited[x1][y1]=true;
+                
                     
                 }
             }
