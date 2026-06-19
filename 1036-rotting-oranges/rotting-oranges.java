6156c921e71e06eb83f18011c22cf2a5e5fb1189
Harshil Sharma <228211535+HarshilS31@users.noreply.github.com>
2026-06-19 12:33:02 +0530

    Time: 2 ms (90.87%) | Memory: 44.4 MB (14.31%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,47 +1,37 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        if (grid == null || grid.length == 0) return 0;
-        int rows = grid.length;
-        int cols = grid[0].length;
-        Queue<int[]> queue = new LinkedList<>();
-        int freshOranges = 0;
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (grid[r][c] == 2) {
-                    queue.add(new int[]{r, c});
-                } else if (grid[r][c] == 1) {
-                    freshOranges++;
-                }
-            }
-        }
-        if (freshOranges == 0) return 0;
-        int minutes = 0;
-        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            boolean rottedThisMinute = false;
-            
-            for (int i = 0; i < size; i++) {
-                int[] curr = queue.poll();
-                int r = curr[0];
-                int c = curr[1];
-                
-                for (int[] dir : directions) {
-                    int nr = r + dir[0];
-                    int nc = c + dir[1];
-                    if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == 1) {
-                        grid[nr][nc] = 2; 
-                        queue.add(new int[]{nr, nc});
-                        freshOranges--; 
-                        rottedThisMinute = true;
-                    }
-                }
-            }
-            if (rottedThisMinute) {
-                minutes++;
+        int time = 0;
+        int n = grid.length;
+        int m = grid[0].length;
+        int fresh = 0;
+
+        Queue<int[]> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) q.add(new int[]{i, j, 0});
+                if (grid[i][j] == 1) fresh++;
             }
         }
 
-        return freshOranges == 0 ? minutes : -1;
+        int[][] directions = {{0,1},{0,-1},{1,0},{-1,0}};
+
+        while (q.size() > 0) {
+            int[] top = q.poll();
+            int x = top[0];
+            int y = top[1];
+            int t = top[2];
+            time = t > time ? t : time;
+
+            for (int[] d : directions) {
+                int x1 = x + d[0];
+                int y1 = y + d[1];
+                if (x1 >= 0 && y1 >= 0 && x1 < n && y1 < m && grid[x1][y1] == 1) {
+                    grid[x1][y1] = 2;
+                    fresh--;
+                    q.add(new int[]{x1, y1, t + 1});
+                }
+            }
+        }
+        return fresh == 0 ? time : -1;
     }
 }
