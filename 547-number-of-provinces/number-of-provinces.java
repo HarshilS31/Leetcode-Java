3413342d7b40a1ab468980741b92554be5b72918
Harshil Sharma <228211535+HarshilS31@users.noreply.github.com>
2026-06-19 07:40:09 +0530

    Time: 3 ms (27.06%) | Memory: 47.2 MB (57.46%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -2,14 +2,18 @@ class Solution {
     public int findCircleNum(int[][] adj) {
         int count=0;
         int n=adj.length;
+        int[] size= new int[n+1];
         int[] parent = new int[n+1]; 
-        for(int i=1;i<=n;i++) parent[i]=i;
+        for(int i=1;i<=n;i++) {
+            parent[i]=i;
+            size[i]=1;
+        }
         parent[0]=Integer.MIN_VALUE;
         List<List<Integer>> edges = new ArrayList<>();
         for(int i=0;i<n;i++) {
             for(int j=0;j<n;j++) { 
                 if(adj[i][j]==1 && i!=j) {
-                    union(i+1,j+1,parent);
+                    union(i+1,j+1,parent,size);
                 } 
             }
         }
@@ -19,13 +23,18 @@ class Solution {
         return count;
         
     }
-    public void union(int x,int y,int[] parent) {
-        int ultimateP_x=find(x,parent);
-        int ultimateP_y=find(y,parent);
-        if(ultimateP_x!=ultimateP_y) {
-            parent[ultimateP_y]=ultimateP_x;
-
+    public void union(int x,int y,int[] parent,int[] size) {
+        x=find(x,parent);
+        y=find(y,parent);
+        if (size[x]>=size[y]) {
+            parent[y]=x;
+            size[x]+=size[y];
+        }     
+        else if(size[y]>size[x]){
+            parent[x]=y;
+            size[y]+=size[x];
         }
+  
 
     }
     public static int find(int node,int[] parent) {
