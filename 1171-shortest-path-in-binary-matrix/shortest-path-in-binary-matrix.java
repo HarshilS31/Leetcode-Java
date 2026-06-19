6c243d9224053edf95c9c9721c195ee1a196fb26
Harshil Sharma <228211535+HarshilS31@users.noreply.github.com>
2026-06-19 16:31:03 +0530

    Time: 35 ms (6.55%) | Memory: 47.7 MB (59.05%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -7,22 +7,26 @@ class Solution {
         for(int i=0;i<n;i++) Arrays.fill(distance[i],Integer.MAX_VALUE);
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->Integer.compare(a[2],b[2]));
         int[][] directions ={{0,1},{0,-1},{1,0},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
+        boolean[][] visited = new boolean[n][n];
         pq.add(new int[]{0,0,1});
+        visited[0][0]=true;
         while(pq.size()>0) {
             int[] top = pq.poll();
             int x=top[0];
             int y=top[1];
             int d=top[2];
+            
             for(int[] dir:directions) {
                 int x1=x+dir[0];
                 int y1=y+dir[1];
                 
-                if(x1>=0 && y1>=0 && x1<n && y1<n && grid[x1][y1]==0) {
+                if(x1>=0 && y1>=0 && x1<n && y1<n && grid[x1][y1]==0 && !visited[x1][y1]) {
                     int dist=d+1;
                     if(distance[x1][y1]>dist) {
                         distance[x1][y1]=dist;
                         pq.add(new int[]{x1,y1,dist});
                     }
+                    visited[x1][y1]=true;
                     
                 }
             }
