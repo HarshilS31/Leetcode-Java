d66bd456e15057ee81d64e5e6b687361cd2c7796
Harshil Sharma <harshils3107@gmail.com>
2026-06-28 11:38:56 +0530

    Time: 11 ms (94.96%) | Memory: 44 MB (74.89%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -1,53 +1,50 @@
 class Solution {
+    void findParent(TreeNode root,HashMap<TreeNode,TreeNode> map) {
+        if(root==null) return;
+        if(root.left!=null) map.put(root.left,root);
+        findParent(root.left,map);
+        if(root.right!=null) map.put(root.right,root);
+        findParent(root.right,map);
+
+
+    }
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        HashMap<TreeNode, TreeNode> map = new HashMap<>();
-        findParent(map, root);
         List<Integer> ans = new ArrayList<>();
         HashSet<TreeNode> visited = new HashSet<>();
+        HashMap<TreeNode,TreeNode> map = new HashMap<>();
+        findParent(root,map);
         Queue<TreeNode> q = new LinkedList<>();
-        
-        visited.add(target);
+        int level=0;
         q.add(target);
-        int level = 0;
-        while(!q.isEmpty()) {
-            int size = q.size();
-            if (level == k) break; 
-            
-            for(int i = 0; i < size; i++) {
-                TreeNode curr = q.poll();
+        visited.add(target);;
+        while(q.size()>0) {
+            int size=q.size();
+            if(level==k) break;
+            for(int i=0;i<size;i++) {
+                TreeNode curr=q.poll();
                 TreeNode parent = map.get(curr);
-                if(curr.left != null && !visited.contains(curr.left)) {
+                if(curr.left!=null && !visited.contains(curr.left)) {
                     q.add(curr.left);
                     visited.add(curr.left);
                 }
-                if(curr.right != null && !visited.contains(curr.right)) {
+                if(curr.right!=null && !visited.contains(curr.right)) {
                     q.add(curr.right);
                     visited.add(curr.right);
                 }
-                if(parent != null && !visited.contains(parent)) {
+                if(parent!=null && !visited.contains(parent)) {
                     q.add(parent);
                     visited.add(parent);
                 }
             }
             level++;
+
         }
         while(!q.isEmpty()) {
-            ans.add(q.poll().val);
+            TreeNode curr=q.poll();
+            ans.add(curr.val);
         }
-        
         return ans;
-    }
 
-    void findParent(HashMap<TreeNode, TreeNode> map, TreeNode node) {
-        if(node == null) return;
         
-        if(node.left != null) {
-            map.put(node.left, node);
-            findParent(map, node.left);
-        }
-        if(node.right != null) {
-            map.put(node.right, node);
-            findParent(map, node.right);
-        }
     }
 }
