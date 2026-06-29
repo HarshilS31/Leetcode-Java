4f41af460973496c7348e7e8b6b5e2bd27568873
Harshil Sharma <harshils3107@gmail.com>
2026-06-29 20:42:20 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (17.74%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -1,32 +1,26 @@
 class Solution {
-    public TreeNode deleteNode(TreeNode root, int target) {
-        return deleteTreeNode(root, target);
+    public TreeNode deleteNode(TreeNode root, int key) {
+        return delNode(root, key);
     }
-
-    TreeNode deleteTreeNode(TreeNode root, int target) {
-        if (root == null) return null;
-        
-        if (root.val > target) {
-            root.left = deleteTreeNode(root.left, target);
-        } else if (root.val < target) {
-            root.right = deleteTreeNode(root.right, target);
+    public TreeNode delNode(TreeNode node, int key) {
+        if (node == null) return null;
+        if (node.val > key) {
+            node.left = delNode(node.left, key); 
+        } else if (node.val < key) {
+            node.right = delNode(node.right, key); 
         } else {
-            // No children
-            if (root.left == null && root.right == null) {
-                return null;
-            }
-            // One child
-            else if (root.left == null) return root.right;
-            else if (root.right == null) return root.left;
-            // Two children
+            if (node.left == null) return node.right; 
+            else if (node.right == null) return node.left;
             else {
-                TreeNode pred = root.left;
-                while (pred.right != null) pred = pred.right;
-                root.val = pred.val;
-                root.left = deleteTreeNode(root.left, pred.val);
-                return root;
+
+                TreeNode temp = node.right;
+                while (temp.left != null) {
+                    temp = temp.left;
+                }
+                temp.left = node.left; 
+                return node.right; 
             }
         }
-        return root;
+        return node; 
     }
 }
