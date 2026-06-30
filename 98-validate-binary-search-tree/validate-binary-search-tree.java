7a8eee1f9fa4b0ff64f65e664d0685f11c56a61a
Harshil Sharma <harshils3107@gmail.com>
2026-06-30 12:06:38 +0530

    Time: 2 ms (11.81%) | Memory: 45.6 MB (7.10%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -1,38 +1,15 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-
-    public boolean isValidBST(TreeNode root) {
-        ArrayList<Integer> arr= new ArrayList<>();
-        attach(root,arr);
-        for (int i = 0; i < arr.size() - 1; i++) {
-        if (arr.get(i) >= arr.get(i + 1)) {
-            return false;
-        }
-    }
+    boolean validBST=true;
+    public boolean isValidBST(TreeNode root) { //O(N) space
+        ArrayList<Integer> arr = new ArrayList<>();
+        inorder(root,arr);
+        for(int i=1;i<arr.size();i++) if(arr.get(i-1)>=arr.get(i)) return false;
         return true;
-
-
-
-
-    }
-    public static void attach(TreeNode root,ArrayList<Integer> arr) {
+    }   
+    public void inorder(TreeNode root,ArrayList<Integer> arr) {
         if(root==null) return;
-        attach(root.left,arr);
+        inorder(root.left,arr);
         arr.add(root.val);
-        attach(root.right,arr);
+        inorder(root.right,arr);
     }
 }
