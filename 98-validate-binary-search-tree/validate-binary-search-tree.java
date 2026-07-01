3adc9da87f2610bf4799f1205a0c9ee36607f6f9
Harshil Sharma <harshils3107@gmail.com>
2026-07-01 12:44:54 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (14.01%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -1,16 +1,13 @@
 class Solution {
     Integer prev=null;
-    public boolean isValidBST(TreeNode root) { 
-        return validateBST(root);
-
-
-    }   
-    public boolean validateBST(TreeNode node) {
-        if(node==null) return true;
-        if(!validateBST(node.left)) return false;
+    public boolean isValidBST(TreeNode root) {
+        return checkBST(root);
+    }
+    public boolean checkBST(TreeNode node) {
+        if(node==null) return  true;
+        if(!checkBST(node.left)) return false;
         if(prev!=null && node.val<=prev) return false;
         prev=node.val;
-        return validateBST(node.right);
+        return checkBST(node.right);
     }
-
 }
