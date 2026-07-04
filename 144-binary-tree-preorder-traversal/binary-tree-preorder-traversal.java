2632dcfde5c561a98c1551b424a9e39f925628f4
Harshil Sharma <harshils3107@gmail.com>
2026-07-04 09:36:51 +0530

    Time: 1 ms (6.80%) | Memory: 43.4 MB (14.93%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -1,16 +1,14 @@
-
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        //Iterative Preorder Traversal
+        Stack<TreeNode> s = new Stack<>();
         List<Integer> ans = new ArrayList<>();
         if(root==null) return ans;
-        Stack<TreeNode> s = new Stack<>();
         s.push(root);
         while(!s.isEmpty()) {
-            TreeNode top=s.pop();
-            ans.add(top.val);
-            if(top.right!=null) s.push(top.right);
-            if(top.left!=null) s.push(top.left);
+            TreeNode curr=s.pop();
+            ans.add(curr.val);
+            if(curr.right!=null) s.push(curr.right);
+            if(curr.left!=null) s.push(curr.left);
         }
         return ans;
     }
