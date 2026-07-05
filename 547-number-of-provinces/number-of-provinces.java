e0f45a53e36405d12014e37f35ff4b81ab518430
Harshil Sharma <harshils3107@gmail.com>
2026-07-05 13:31:56 +0530

    Time: 1 ms (87.13%) | Memory: 47.2 MB (73.57%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,45 +1,22 @@
 class Solution {
-    public int findCircleNum(int[][] adj) {
-        int count=0;
-        int n=adj.length;
-        int[] size= new int[n+1];
-        int[] parent = new int[n+1]; 
-        for(int i=1;i<=n;i++) {
-            parent[i]=i;
-            size[i]=1;
-        }
-        parent[0]=Integer.MIN_VALUE;
-        List<List<Integer>> edges = new ArrayList<>();
+    public int findCircleNum(int[][] isConnected) {
+        //DFS
+        int n = isConnected.length;
+        int provinces =0;
+        boolean[] visited = new boolean[n];
         for(int i=0;i<n;i++) {
-            for(int j=0;j<n;j++) { 
-                if(adj[i][j]==1 && i!=j) {
-                    union(i+1,j+1,parent,size);
-                } 
+            if(!visited[i]) {
+                dfs(isConnected,visited,i);
+                provinces++;
             }
         }
-        for(int i=1;i<=n;i++) {
-            if(parent[i]==i) count++;
-        }
-        return count;
-        
+        return provinces;      
     }
-    public void union(int x,int y,int[] parent,int[] size) {
-        x=find(x,parent);
-        y=find(y,parent);
-        if (size[x]>=size[y]) {
-            parent[y]=x;
-            size[x]+=size[y];
-        }     
-        else if(size[y]>size[x]){
-            parent[x]=y;
-            size[y]+=size[x];
+    public void dfs(int[][] isConnected,boolean[] visited,int i) {
+        visited[i]=true;
+        for(int j=0;j<isConnected.length;j++) if(isConnected[i][j]==1 && !visited[j]) {
+            dfs(isConnected,visited,j);
+            
         }
-  
-
-    }
-    public static int find(int node,int[] parent) {
-        if(node==parent[node]) return node;
-        return parent[node] = find(parent[node],parent);
-
     }
 }
