122c77c6bd24820b8da5285f957e71b5c0e2d08e
Harshil Sharma <harshils3107@gmail.com>
2026-07-07 11:40:08 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (96.12%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,24 +1,16 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp= new int[m][n];
-        return Paths(0,0,m,n,dp);
+        if(m==1 || n==1) return 1;
+        int[][] memo = new int[m][n];
+        for(int[] arr:memo) Arrays.fill(arr,-1);
+        findPaths(memo,m-1,n-1,m,n);
+        return memo[m-1][n-1];
+    }
+    public int findPaths(int[][] memo,int i,int j,int m,int n) {
+        if(i>=m || j>=n ) return 0;
+        if(i==0 || j==0 ) return 1;
+        if(memo[i][j]!=-1) return memo[i][j];
+        return memo[i][j]=findPaths(memo,i-1,j,m,n)+findPaths(memo,i,j-1,m,n);
+    }
 
-
-  }
-  public int Paths(int r,int c,int m,int n,int[][] dp) {
-    if(r>=m ||  c>=n) return 0;
-    else if(r==m-1 || c==n-1) return 1;
-    if (dp[r][c]!=0) return dp[r][c];
-    else dp[r][c]=Paths(r+1,c,m,n,dp)+Paths(r,c+1,m,n,dp);
-    return dp[r][c];
-
-
-  }
 }
-      
-
-        
-
-       
-
-        
