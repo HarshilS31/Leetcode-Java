0aa409b0cfbdf2faa3baa426002fc4a108a6f38a
Harshil Sharma <harshils3107@gmail.com>
2026-07-11 11:36:25 +0530

    Time: 7 ms (57.03%) | Memory: 47.2 MB (49.71%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,36 +1,37 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<Integer>[] adj = new ArrayList[numCourses];
-        for (int i = 0; i < numCourses; i++) {
-            adj[i] = new ArrayList<>();
+    public boolean canFinish(int n, int[][] pre) {
+        List<List<Integer>> adj = new ArrayList<>();
+        boolean[] visited = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> ans = new ArrayList<>();
+        int[] indegree = new int[n];
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+        for(int i=0;i<pre.length;i++) {
+            int x = pre[i][0];
+            int y = pre[i][1];
+            adj.get(y).add(x);
+            indegree[x]++;
         }
-        for (int[] edge : prerequisites) {
-            adj[edge[1]].add(edge[0]);
+        for(int i=0;i<n;i++)  {
+            if(indegree[i]==0) {
+                q.add(i);
+                visited[i]=true;
+            }
         }
-
-        // State array: 0 -> unvisited, 1 -> visiting, 2 -> visited
-        int[] state = new int[numCourses];
-        for (int i = 0; i < numCourses; i++) {
-            if (state[i] == 0) {
-                if (hasCycle(adj, state, i)) {
-                    return false; 
+        while(q.size()>0) {
+            int top = q.poll();
+            ans.add(top);
+            for(int node : adj.get(top)) {
+                indegree[node]--;
+                if(indegree[node]==0) {
+                    q.add(node);
+                    visited[node]=true; 
                 }
+                
             }
+            
         }
-        return true;
-    }
-
-    private boolean hasCycle(List<Integer>[] adj, int[] state, int curr) {
-        state[curr]=1;
-        for(int node:adj[curr]) {
-            if(state[node]==1) return true;
-            else if(state[node]==0) {
-                if(hasCycle(adj,state,node)) return true;
-            }
-        }
-        state[curr]=2;
-        return false;
-
-
+        return ans.size()==n;
+        
     }
 }
