2dfa4ed7e82f4f89d218bb3448c361b1b9e1e112
Harshil Sharma <harshils3107@gmail.com>
2026-07-13 14:11:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (73.12%) - LeetSync

--- 1212-sequential-digits/sequential-digits.java
+++ 1212-sequential-digits/sequential-digits.java
@@ -1,10 +1,8 @@
 class Solution {
     public List<Integer> sequentialDigits(int low, int high) {
         String s = "123456789";
-        // O(1)
         int minlen = String.valueOf(low).length();
         int maxlen = String.valueOf(high).length();
-
         List<Integer> ans = new ArrayList<>();
         for(int i=minlen; i<=maxlen; i++) {
             for(int start=0; start+i<=9; start++) {
