180985bad23315508b5684edcac9676484674367
Harshil Sharma <harshils3107@gmail.com>
2026-07-13 12:26:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (54.52%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,15 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return memo(n,dp);
+        if(n<=1) return n;
+        int n1=0;
+        int n2=1;
+        int n3=0;
+        for(int i=1;i<=n;i++) {
+            n3=n1+n2;
+            n1=n2;
+            n2=n3;
+        }
+        return n3;
+
         
     }
-    public int memo(int i,int[] dp) {
-        if(i<=2) return i;
-        if(dp[i]!=-1) return dp[i];
-        else return dp[i]=memo(i-1,dp)+memo(i-2,dp);
-    }
-    
-
 }
