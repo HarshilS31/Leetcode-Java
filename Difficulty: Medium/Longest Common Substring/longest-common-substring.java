6c47026d81ecb8a3d30310e110c36647fbd15aba
Harshil Sharma <harshils3107@gmail.com>
2026-07-17 11:14:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.java
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.java
@@ -1,23 +1,20 @@
 class Solution {
     public int longCommSubstr(String s1, String s2) {
-       int m = s1.length();
-       int n = s2.length();
-       int[][] dp = new int[m+1][n+1];
-       for(int i=1;i<=m;i++) {
-           for(int j=1;j<=n;j++) {
-               if(s1.charAt(i-1)==s2.charAt(j-1)) dp[i][j]=1+dp[i-1][j-1];
-               else dp[i][j]=0;
-           }
-       }
-       int max=0;
-       for(int i=1;i<=m;i++) {
-           for(int j=1;j<=n;j++) {
-               if(dp[i][j]>max) max=dp[i][j];
-               
-           }
-       } 
-       return max;
-       
-        
+    int n = s1.length();
+    int m = s2.length();
+    int[][] dp = new int[n + 1][m + 1];
+    int maxLen = 0;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+                maxLen = Math.max(maxLen, dp[i][j]);
+            } else {
+                dp[i][j] = 0; // Explicitly resetting the streak
+            }
+        }
+    }
+    return maxLen;
     }
 }
