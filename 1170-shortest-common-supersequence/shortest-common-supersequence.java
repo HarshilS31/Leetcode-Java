fc80ae2307b43a89a1720cd351c2a5bafce0ee87
Harshil Sharma <harshils3107@gmail.com>
2026-07-19 12:34:43 +0530

    Time: 24 ms (44.31%) | Memory: 54 MB (49.63%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.java
@@ -1,49 +1,41 @@
 class Solution {
-    public String shortestCommonSupersequence(String a, String b) {
-        String lcs = LCS(a, b);
-        int i = 0, j = 0, k = 0;
+    public String shortestCommonSupersequence(String str1, String str2) {
+        int m=str1.length();
+        int n=str2.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i=1;i<=m;i++) {
+            for(int j=1;j<=n;j++) {
+                if(str1.charAt(i-1)==str2.charAt(j-1)) dp[i][j]= 1+dp[i-1][j-1];
+                else dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);    
+            }
+        }
         StringBuilder scs = new StringBuilder();
-        while (i < a.length() && j < b.length() && k < lcs.length()) {
-            while (i < a.length() && a.charAt(i) != lcs.charAt(k)) scs.append(a.charAt(i++));
-            while (j < b.length() && b.charAt(j) != lcs.charAt(k)) scs.append(b.charAt(j++));
-            scs.append(lcs.charAt(k));
-            i++;
-            j++;
-            k++;
-        }
-        while (i < a.length()) scs.append(a.charAt(i++));
-        while (j < b.length()) scs.append(b.charAt(j++));  
-        return scs.toString();
-    }
-    
-    public String LCS(String str1, String str2) {
-        int m = str1.length();
-        int n = str2.length();
-        int[][] dp = new int[m + 1][n + 1];
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
-                }
+        int i=m;
+        int j=n;
+        while(i>0 && j>0) {
+            if(str1.charAt(i-1)==str2.charAt(j-1)) {
+                scs.append(str1.charAt(i-1));
+                i--;
+                j--;     
+            }
+            else if (dp[i][j-1]>dp[i-1][j]) {
+                scs.append(str2.charAt(j-1));
+                j--;  
+            }
+            else {
+                scs.append(str1.charAt(i-1));
+                i--;
             }
         }
-
-        StringBuilder lcsBuilder = new StringBuilder();
-        int i = m, j = n;
-        while (i > 0 && j > 0) {
-            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-                lcsBuilder.append(str1.charAt(i - 1));
-                i--;
-                j--;
-            } else if (dp[i - 1][j] > dp[i][j - 1]) {
-                i--;
-            } else {
-                j--;
-            }
+        while(i>0){
+            scs.append(str1.charAt(i-1));
+            i--;
         }
-        
-        return lcsBuilder.reverse().toString();
+        while(j>0){
+            scs.append(str2.charAt(j-1));
+            j--;
+        }
+        scs.reverse();
+        return scs.toString();  
     }
 }
