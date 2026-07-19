3e329941b2a5ff063a2fd8fb92447a1dc85ab2bb
Harshil Sharma <harshils3107@gmail.com>
2026-07-19 14:03:25 +0530

    Time: 21 ms (35.58%) | Memory: 54.2 MB (64.83%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -2,17 +2,16 @@ class Solution {
     public int numDistinct(String s, String t) {
         int n=s.length();
         int m=t.length();
-        int[][] dp = new int[n][m];
-        for(int[] arr:dp) Arrays.fill(arr,-1);
-        return findWays(s,t,dp,n-1,m-1);
-    }
-    public int findWays(String s,String t,int[][] dp,int i,int j) {
-        if(j<0) return 1;
-        if(i<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s.charAt(i)==t.charAt(j)) {
-            return dp[i][j]=findWays(s,t,dp,i-1,j-1)+findWays(s,t,dp,i-1,j);  
+        int[][] dp = new int[n+1][m+1];
+        for(int i=0;i<=n;i++) dp[i][0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++) {
+                if(s.charAt(i-1)==t.charAt(j-1)) {
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else dp[i][j]=dp[i-1][j];
+            }
         }
-        return dp[i][j]=findWays(s,t,dp,i-1,j);
+        return dp[n][m]; 
     }
 }
