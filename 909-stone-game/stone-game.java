857d3f9342dffc7784cd760021822467c0ed9558
Harshil Sharma <harshils3107@gmail.com>
2026-08-09 16:51:34 +0530

    Time: 10 ms (13.00%) | Memory: 47 MB (12.31%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,8 +1,15 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
-        
-
-        
+        int n =piles.length;
+        int[][] dp = new int[n][n];
+        for(int[] arr :dp) Arrays.fill(arr,-1);
+        return diff(piles,dp,0,n-1)>0;
+    }
+    public int diff(int[] piles,int[][] dp,int i,int j) {
+        if(i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int pickFirst = piles[i]-diff(piles,dp,i+1,j);
+        int pickLast = piles[j]-diff(piles,dp,i,j-1);
+        return dp[i][j]=Math.max(pickFirst,pickLast);
     }
 }
