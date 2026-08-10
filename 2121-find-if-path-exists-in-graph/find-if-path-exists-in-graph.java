09fc0e060dda21249ea611e4e1122e5a6107d6c3
Harshil Sharma <harshils3107@gmail.com>
2026-08-10 17:49:11 +0530

    Time: 154 ms (30.66%) | Memory: 262.1 MB (85.72%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,33 +1,34 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         if(source==destination) return true;
-        List<List<Integer>> adj = new ArrayList<>();
+        boolean[] visited =  new boolean[n];
+        List<List<Integer>> adj=new ArrayList<>();
         for(int i=0;i<n;i++) {
             List<Integer> list = new ArrayList<>();
             adj.add(list);
         }
-        for(int[] arr: edges) {
-            adj.get(arr[0]).add(arr[1]);
-            adj.get(arr[1]).add(arr[0]);
+        for(int i=0;i<edges.length;i++) {
+            int a =edges[i][0];
+            int b=edges[i][1];
+            adj.get(a).add(b);
+            adj.get(b).add(a);
         }
-        boolean[] visited = new boolean[n];
-        visited[source]=true;
         bfs(source,visited,adj);
-        return visited[destination];   
+        return visited[destination];     
     }
-    public void bfs(int source,boolean[] visited,List<List<Integer>> adj) {
-        Queue<Integer> q = new LinkedList<>();
+    
+    void bfs(int source,boolean[] visited,List<List<Integer>> adj) {
+        Queue<Integer> q= new LinkedList<>();
         q.add(source);
-        while(!q.isEmpty()) {
-            int top=q.poll();
-            for(int edge : adj.get(top)) {
-                if(!visited[edge]) {
-                    visited[edge]=true;
-                    q.add(edge);
+        while(q.size()>0) {
+            int front=q.remove();
+            for(int ele:adj.get(front)) {
+                if(!visited[ele]) {
+                    visited[ele]=true;
+                    q.add(ele);
 
                 }
             }
         }
-       
     }
 }
