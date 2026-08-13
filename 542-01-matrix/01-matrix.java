9a0ee4b581911c326232d2d8cfc57b8ba26ffeef
Harshil Sharma <harshils3107@gmail.com>
2026-08-13 16:37:21 +0530

    Time: 18 ms (12.59%) | Memory: 49.2 MB (51.68%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,34 +1,32 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int r = mat.length;
-        int c =mat[0].length;
-        int[][] ans = new int[r][c];
-        for(int i=0;i<r;i++) Arrays.fill(ans[i],-1);
+        int m=mat.length;
+        int n=mat[0].length;
+        int[][] res = new int[m][n];
+        for(int i=0;i<m;i++) Arrays.fill(res[i],-1);
         Queue<int[]> q = new LinkedList<>();
-        for(int i=0;i<r;i++) {
-            for(int j=0;j<c;j++) {
-                if(mat[i][j]==0 && ans[i][j]==-1) {
-                    ans[i][j]=0;
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(mat[i][j]==0 && res[i][j]==-1) {
+                    res[i][j]=0;
                     q.add(new int[]{i,j});
-
                 }
             }
-        } 
-        int[][] directions ={{1,0},{-1,0},{0,1},{0,-1}}; 
-        while(q.size()>0) {
-            int[] front=q.remove();
+        }
+        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+        while(!q.isEmpty()) {
+            int[] top = q.poll();
+            int x=top[0];
+            int y=top[1];
             for(int[] d:directions) {
-                int i1=front[0]+d[0];
-                int j1=front[1]+d[1];
-                if (i1 >= 0 && j1 >= 0 && i1 < r && j1 < c && mat[i1][j1] == 1 && ans[i1][j1] == -1) {
-                    ans[i1][j1]=ans[front[0]][front[1]]+1;  
-                    q.add(new int[]{i1,j1});  
-
+                int x1=x+d[0];
+                int y1=y+d[1];
+                if(x1>=0 && y1>=0 && x1<m && y1<n && mat[x1][y1]==1 && res[x1][y1]==-1) {
+                    res[x1][y1]=res[x][y]+1;
+                    q.add(new int[]{x1,y1});
                 }
             }
-        
-
-        }  
-        return  ans;    
+        }
+        return res; 
     }
 }
