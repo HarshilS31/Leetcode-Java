c6302e900d52ca336218d2d3b5f769474a863123
Harshil Sharma <harshils3107@gmail.com>
2026-08-13 16:05:43 +0530

    Time: 2 ms (89.70%) | Memory: 45.2 MB (5.53%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,35 +1,33 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        Queue<int[]> q = new LinkedList<>();
-        int time=0;
         int m=grid.length;
-        int n =grid[0].length;
-        int fresh=0;
-        for(int i=0;i<m;i++){
+        int n=grid[0].length;
+        int minTime=0;
+        int freshOranges=0;
+        Queue<int[]> q = new LinkedList<>();
+        for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
                 if(grid[i][j]==2) q.add(new int[]{i,j,0});
-                else if(grid[i][j]==1) fresh++;
+                else if(grid[i][j]==1) freshOranges++;
             }
         }
-        int[][] directions ={{1,0},{-1,0},{0,1},{0,-1}};
         while(!q.isEmpty()) {
             int[] top = q.poll();
             int x=top[0];
             int y=top[1];
-            int t=top[2];
-            time=Math.max(t,time);
-            for(int[] dir : directions) {
-                int x1=x+dir[0];
-                int y1=y+dir[1];
-                if(x1>=0 && y1>=0 && x1<m && y1<n && grid[x1][y1]==1) {
+            int currTime=top[2];
+            minTime=Math.max(minTime,currTime);
+            int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+            for(int[] d:directions) {
+                int x1=x+d[0];
+                int y1= y+d[1];
+                if( x1>=0 && y1>=0 && x1<m && y1<n && grid[x1][y1]==1) {
                     grid[x1][y1]=2;
-                    fresh--;
-                    q.add(new int[]{x1,y1,t+1});
-
+                    freshOranges--;
+                    q.add(new int[]{x1,y1,currTime+1});
                 }
             }
-
         }
-        return fresh==0 ? time :-1;      
+        return freshOranges==0 ? minTime :-1;
     }
 }
