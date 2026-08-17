81d42b010c7f8e74118c0a2c0edb83d03739fc38
Harshil Sharma <harshils3107@gmail.com>
2026-08-17 21:39:55 +0530

    Time: 35 ms (6.03%) | Memory: 48 MB (11.97%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,36 +1,30 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int n= grid.length;
-        if(grid[0].length==1 && grid[0][0]==0) return 1; 
-        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) return -1;
-        int[][] distance = new int[n][n];
-        for(int i=0;i<n;i++) Arrays.fill(distance[i],Integer.MAX_VALUE);
-        Queue<int[]> q = new LinkedList<>();
-        int[][] directions ={{0,1},{0,-1},{1,0},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
-  
-        q.add(new int[]{0,0,1});
-
-        while(q.size()>0) {
-            int[] top = q.poll();
-            int x=top[0];
-            int y=top[1];
-            int d=top[2];
-            
-            for(int[] dir:directions) {
-                int x1=x+dir[0];
-                int y1=y+dir[1];
-                
-                if(x1>=0 && y1>=0 && x1<n && y1<n && grid[x1][y1]==0 ) {
-                    int dist=d+1;
-                    if(distance[x1][y1]>dist) {
-                        distance[x1][y1]=dist;
-                        q.add(new int[]{x1,y1,dist});
+        int m = grid.length;
+        int n = grid[0].length;
+        if(grid[0][0]==1 || grid[m-1][n-1]==1) return -1;
+        int[][] res = new int[m][n];
+        for(int[] arr : res) Arrays.fill(arr,Integer.MAX_VALUE);
+        res[0][0]=1;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->Integer.compare(a[2],b[2]));
+        pq.add(new int[]{0,0,1});
+        int[][] directions = new int[][]{{0,1},{0,-1},{1,0},{-1,0},{1,1},{1,-1},{-1,-1},{-1,1}};
+        while(!pq.isEmpty()) {
+            int[] top = pq.poll();
+            int x = top[0];
+            int y = top[1];
+            int d =top[2];
+            for(int[] dir : directions) {
+                int x1 = x+dir[0];
+                int y1 = y+dir[1];
+                if(x1>=0 && y1>=0 && x1<m && y1<n && grid[x1][y1]==0) {
+                    if(res[x1][y1]>d+1) {
+                        res[x1][y1]=d+1;
+                        pq.add(new int[]{x1,y1,d+1});
                     }
-                
-                    
                 }
             }
         }
-        return distance[n-1][n-1]== Integer.MAX_VALUE ? -1:distance[n-1][n-1];    
+        return res[m-1][n-1] == Integer.MAX_VALUE ? -1 :res[m-1][n-1];
     }
 }
