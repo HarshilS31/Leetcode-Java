e922878a5df191c8ad7a388134370287da6bd742
Harshil Sharma <harshils3107@gmail.com>
2026-08-24 17:10:45 +0530

    Time: 3 ms (86.37%) | Memory: 52.6 MB (5.10%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int candy(int[] ratings) {
+        //Brute force 
+        int n =ratings.length;
+        int[] candies =  new int[n];
+        Arrays.fill(candies,1);
+        for(int i=1;i<n;i++) {
+            if(ratings[i]>ratings[i-1] && candies[i-1]>=candies[i]) {
+                candies[i]+=candies[i-1]-candies[i]+1;
+            }
+        }
+        for(int i=n-2;i>=0;i--) {
+            if(ratings[i]>ratings[i+1] && candies[i+1]>=candies[i]) {
+                candies[i]+=candies[i+1]-candies[i]+1;
+            }
+        }
+        int candyCount=0;
+        for(int candy:candies) candyCount+=candy;
+        return candyCount;
+    }
+}
