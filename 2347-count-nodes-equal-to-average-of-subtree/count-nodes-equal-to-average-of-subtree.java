eb13a097226ebedb8f5f1ef0618df875a1911135
Harshil Sharma <harshils3107@gmail.com>
2026-09-10 16:09:25 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (63.38%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
@@ -1,31 +1,23 @@
 class Solution {
-    int sum;
-    int nodes;
     int avgNodes=0;
     public int averageOfSubtree(TreeNode root) {
         traversal(root);
         return avgNodes;
-        
     }
-    public void traversal(TreeNode node) {
-        if(node==null) return;
-        sum=0;
-        nodes=0;
-        findTreeAvg(node);
+    public int[] traversal(TreeNode node) {
+        if(node==null) return new int[]{0,0};
+        int sum=0;
+        int nodes=0;
+        int[] left  = traversal(node.left);
+        int[] right  = traversal(node.right);
+        sum+=node.val+left[0]+right[0];
+        nodes+=1+left[1]+right[1];
         if(sum/nodes==node.val) avgNodes++;
-        traversal(node.left);
-        traversal(node.right);
-
+        return new int[]{sum,nodes};
     }
-    public void findTreeAvg(TreeNode node) {
-        if(node==null) return;
-        sum+=node.val;
-        nodes++;
-        findTreeAvg(node.left);
-        findTreeAvg(node.right);
 
-        
-    }
+
+
 
 
 }
