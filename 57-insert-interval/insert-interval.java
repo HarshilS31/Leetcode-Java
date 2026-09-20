e4ac61f3310bc408b14df2fb9865897ab5da0cd0
Harshil Sharma <harshils3107@gmail.com>
2026-09-20 18:16:30 +0530

    Time: 1 ms (97.96%) | Memory: 47.2 MB (37.54%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -2,36 +2,19 @@ class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         int n = intervals.length;
         List<int[]> list = new ArrayList<>();
+        int i=0;
+        while(i<n && newInterval[0]>intervals[i][1]) {
+            list.add(intervals[i++]);
+        }
+        while(i<n && intervals[i][0] <= newInterval[1])  {
+            newInterval[0]=Math.min(intervals[i][0],newInterval[0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
         list.add(newInterval);
-        for(int[] interval:intervals) {
-            list.add(interval);
+        while(i<n) {
+            list.add(intervals[i++]);
         }
-        return merge(list.toArray(new int[list.size()][2]));
-    
-    }
-    public int[][] merge(int[][] intervals) {
-        int n = intervals.length;
-        if(n==1) return intervals;
-        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
-        List<int[]> res = new ArrayList<>();
-        int[] curr = intervals[0];
-        for(int[] interval: intervals) {
-            int currStart = curr[0];
-            int currEnd = curr[1];
-            int nextStart = interval[0];
-            int nextEnd = interval[1];
-            if(currEnd >= nextStart) {
-                curr[1] = Math.max(currEnd,nextEnd);
-            }
-            else {
-                res.add(curr);
-                curr=interval;
-            }
-        }
-        res.add(curr);
-        return res.toArray(new int[res.size()][2]);
-
-
-        
+        return list.toArray(new int[list.size()][2]);   
     }
 }
